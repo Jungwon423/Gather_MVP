802404f60f13235da4dafe757504207b232a304f
Jungwon423 <jungwon423@naver.com>
2023-05-18 01:54:04 +0900

    초기버전

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,11 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:gather_mvp/Cafe.dart';
 import 'package:gather_mvp/HomePage.dart';
 
 final Map<String, WidgetBuilder> route = {
-  "/main" : (context) => HomePage(),
+  "/home" : (context) => HomePage(),
+  '/cafe' : (context) => Cafe()
 
 };
