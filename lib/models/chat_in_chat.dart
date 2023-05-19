54dee696b92a252d7fe49d0aeff82bff9e679675
Jungwon423 <jungwon423@naver.com>
2023-05-19 17:38:20 +0900

    dart : io 수정

--- lib/models/chat_in_chat.dart
+++ lib/models/chat_in_chat.dart
@@ -1,10 +1,10 @@
 class ChatInChat {
-  DateTime dateTime;
-  String chat;
-  String sender;
-  bool isWaiting;
-  List<String> helpText;
-  bool tipExist;
+  DateTime dateTime; // 정렬 용도
+  String chat; // 대화 내용
+  String sender; // 보낸 사람
+  bool isWaiting; // 대기 중 여부
+  List<String> helpText; // TIP -> 일단 무시
+  bool tipExist; // TIP 존재 여부
 
   ChatInChat(this.dateTime, this.chat, this.sender, this.isWaiting, this.helpText, this.tipExist);
 }
